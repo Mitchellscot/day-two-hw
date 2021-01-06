1f4b124e7731f67cdf74973d869b632c8758cec8
mitchellscot <Mitchellscott@me.com>
2021-01-05 20:57:49 -0600

    got it done

--- client.js
+++ client.js
@@ -11,14 +11,21 @@ let refrigeratorContents  = {
 // You shouldn't need to change anything above this line initially
 // You may change it to test different scenarios, but it should work as is
 
-function breakfastChecker(contents) {
+function breakfastChecker(contents, people) {
   // Your code goes here
-
-
-
-
-
+  const baconNeeded = people*3;
+  const eggsNeeded = people*2;
+  const pancakeBagsNeeded = people*.3;
+  const chocolateNeeded = people*45;
+  let enoughFood = false;
+  if (contents.baconStrips >= baconNeeded && contents.eggs >= eggsNeeded) {
+    enoughFood = true;
+  }
+  if (contents.pancakeMixBags >= pancakeBagsNeeded && contents.chocolateChips >= chocolateNeeded) {
+    enoughFood = true;
+  }
+  return enoughFood;
 }
 
 // Your function should return 'yes' or 'no'
-console.log('Does the refrigerator have enough food for breakfast?', breakfastChecker(refrigeratorContents));
+console.log('Does the refrigerator have enough food for breakfast?', breakfastChecker(refrigeratorContents, 4));
